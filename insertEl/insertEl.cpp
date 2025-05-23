7e7ce62c42c4e74fccf18afd4f5b56ecff3f2230
WindowsM16a <wuvershayne@gmail.com>
2025-05-23 05:16:48 +0000

    ass2

---
+++ insertEl/insertEl.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>;
+using namespace std;
+
+// Write a c++ code to insert an element into an array also perform an analysis of the algorithm that you write.
+
+int main()
+{
+    cout << "hey" << endl;
+}
