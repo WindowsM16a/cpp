b53104f7469abb3bc66343989022d50c5f37ed47
WindowsM16a <wuvershayne@gmail.com>
2025-05-24 18:39:37 +0000

    inserting user input into an array

--- insertEl/insertEl.cpp
+++ insertEl/insertEl.cpp
@@ -1,5 +1,4 @@
-
-#include <iostream>;
+#include <iostream>
 using namespace std;
 
 // Write a c++ code to insert an element into an array also perform an analysis of the algorithm that you write.
@@ -7,4 +6,24 @@ using namespace std;
 int main()
 {
     cout << "hey" << endl;
+
+    int myArray[5] = {2, 5, 3, 7, 4};
+    int position, value;
+
+    int myArrayLength = sizeof(myArray) / sizeof(myArray[0]);
+
+    cout << "Enter a value to insert" << endl;
+
+    cin >> value;
+
+    cout << "Enter the position to put" + value << endl;
+
+    cin >> position;
+
+    myArray[position] = value;
+
+    for (int i = 0; i < myArrayLength; i++)
+    {
+        cout << myArray[i] << endl;
+    }
 }
