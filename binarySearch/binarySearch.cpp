502b18a305a81bb2f3d9f161f4e4fc2862a01605
WindowsM16a <wuvershayne@gmail.com>
2025-05-24 23:57:27 +0000

    binary search done

---
+++ binarySearch/binarySearch.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// Write a c++ for implementation for a binary search Algorithm
+
+void binarySearch(int target);
+
+int main()
+{
+    cout << "hey" << endl;
+    int searchValue;
+    cout << "Enter the value whose index you want to find" << endl;
+    cin >> searchValue;
+    binarySearch(searchValue);
+}
+
+void binarySearch(int target)
+{
+    int myArray[] = {1, 2, 3, 5, 7, 6, 4, 8, 9, 10, 11};
+    int myArrayLength = sizeof(myArray) / sizeof(myArray[0]);
+    bool found = false;
+
+    sort(myArray, myArray + myArrayLength);
+
+    int start = 0;
+    int end = myArrayLength - 1;
+
+    while (start <= end)
+    {
+        int middle = (start + end) / 2;
+
+        if (myArray[middle] == target)
+        {
+            cout << "Found target index: " << middle << endl;
+            return;
+        }
+        else if (myArray[middle] > target)
+        {
+            end = middle - 1;
+        }
+        else
+        {
+            start = middle + 1;
+        }
+    }
+    cout << "Target: " << target << ", not found in the array" << endl;
+}
